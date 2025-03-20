700a5b419979a1c576363489a3d82f0f781faee6
suvarnaks1 <suvarnaks2002@gmail.com>
2025-03-20 09:24:49 +0530

    desktop

--- lib/desktop_layout/screens/desktop_layout_screen.dart
+++ lib/desktop_layout/screens/desktop_layout_screen.dart
@@ -16,7 +16,16 @@ class DesktopLayoutScreen extends StatefulWidget {
 }
 
 class _DesktopLayoutScreenState extends State<DesktopLayoutScreen> {
+  // ScrollController to control scrolling
+  final ScrollController _scrollController = ScrollController();
 
+  // Global keys to link sections with AppBar
+  final GlobalKey _aboutMeKey = GlobalKey();
+  final GlobalKey _myServicesKey = GlobalKey();
+  final GlobalKey _myProjectsKey = GlobalKey();
+  final GlobalKey _trainingKey = GlobalKey();
+  final GlobalKey _contactKey = GlobalKey();
+  final GlobalKey _addressKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -34,112 +43,80 @@ class _DesktopLayoutScreenState extends State<DesktopLayoutScreen> {
             SizedBox(width: 20),
             Text('Dot Dart', style: GoogleFonts.bebasNeue(color: AppColors.white)),
             Spacer(),
-            Text('About Me', style: GoogleFonts.bebasNeue(color: AppColors.themeColor)),
+            _buildAppBarText('About Me', _aboutMeKey),
             SizedBox(width: 30),
-            Text('My Services', style: GoogleFonts.bebasNeue(color: AppColors.themeColor)),
+            _buildAppBarText('My Services', _myServicesKey),
             SizedBox(width: 30),
-            Text('My Projects', style: GoogleFonts.bebasNeue(color: AppColors.themeColor)),
+            _buildAppBarText('My Projects', _myProjectsKey),
             SizedBox(width: 30),
-            Text('Training', style: GoogleFonts.bebasNeue(color: AppColors.themeColor)),
-           
+            _buildAppBarText('Training', _trainingKey),
             SizedBox(width: 30),
-            Text('Contact Me', style: GoogleFonts.bebasNeue(color: AppColors.themeColor)),
+            _buildAppBarText('Contact Me', _contactKey),
           ],
         ),
       ),
       body: SingleChildScrollView(
+        controller: _scrollController,
         child: Column(
-          children: [AboutMe(), MyServices(), MyProjects(),TrainingPage(),
-           ContactScreen(),
-          AdressPage()],
+          children: [
+            _buildSection(_aboutMeKey, AboutMe()),
+            _buildSection(_myServicesKey, MyServices()),
+            _buildSection(_myProjectsKey, MyProjects()),
+            _buildSection(_trainingKey, TrainingPage()),
+            _buildSection(_contactKey, ContactScreen()),
+            _buildSection(_addressKey, AdressPage()),
+          ],
         ),
       ),
- floatingActionButton: SizedBox(
-  width: 150, // Set your desired width
-  height: 50, // Optional: Adjust height
-  child: FloatingActionButton.extended(
-    onPressed: () {
-     
-    },
-    backgroundColor: AppColors.lawGreen,
-    label: Text(
-      'Online Courses',
-      style: TextStyle(color: AppColors.pink, fontWeight: FontWeight.bold),
-    ),
-  ),
-),
-
-    ); 
-  }
-
-
-  
-}
-
-
-
-class MyAppBar extends StatefulWidget {
-  const MyAppBar({super.key});
-
-  @override
-  _MyAppBarState createState() => _MyAppBarState();
-}
-
-class _MyAppBarState extends State<MyAppBar> {
-  // Hover state for the text
-  bool isHovered = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      toolbarHeight: 80,
-      backgroundColor: Colors.blueGrey, // AppColors.bgColor2
-      title: Row(
-        children: [
-          CircleAvatar(
-            radius: 30,
-            backgroundImage: AssetImage('assets/dotcanvapic.jpeg'),
+      floatingActionButton: SizedBox(
+        width: 150,
+        height: 50,
+        child: FloatingActionButton.extended(
+          onPressed: () {
+            // Action when the button is clicked
+          },
+          backgroundColor: AppColors.lawGreen,
+          label: Text(
+            'Online Courses',
+            style: TextStyle(color: AppColors.pink, fontWeight: FontWeight.bold),
           ),
-          SizedBox(width: 20),
-          // Hoverable text with animation
-          _buildHoverableText('Dot Dart', isHovered, Colors.white),
-          Spacer(),
-          _buildHoverableText('About Me', isHovered, Colors.blue), // AppColors.themeColor
-          SizedBox(width: 30),
-          _buildHoverableText('My Services', isHovered, Colors.blue), // AppColors.themeColor
-          SizedBox(width: 30),
-          _buildHoverableText('My Projects', isHovered, Colors.blue), // AppColors.themeColor
-          SizedBox(width: 30),
-          _buildHoverableText('Training', isHovered, Colors.blue), // AppColors.themeColor
-          SizedBox(width: 30),
-          _buildHoverableText('Contact Me', isHovered, Colors.blue), // AppColors.themeColor
-        ],
+        ),
       ),
     );
   }
 
-  // Function to build hoverable text
-  Widget _buildHoverableText(String text, bool isHovered, Color color) {
-    return MouseRegion(
-      onEnter: (_) {
-        setState(() {
-          isHovered = true;
-        });
+  // Function to create AppBar text with scroll functionality
+  Widget _buildAppBarText(String text, GlobalKey key) {
+    return GestureDetector(
+      onTap: () {
+        _scrollToSection(key);
       },
-      onExit: (_) {
-        setState(() {
-          isHovered = false;
-        });
-      },
-      child: AnimatedDefaultTextStyle(
-        duration: Duration(milliseconds: 200),
-        style: GoogleFonts.bebasNeue(
-          color: isHovered ? Colors.orange : color, // Hover color change
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
-        child: Text(text),
+      child: Text(
+        text,
+        style: GoogleFonts.bebasNeue(color: AppColors.themeColor),
       ),
     );
+  }
+
+  // Function to wrap sections with GlobalKey reference
+  Widget _buildSection(GlobalKey key, Widget child) {
+    return Container(
+      key: key, // Assign key to container instead of the widget itself
+      child: child,
+    );
+  }
+
+  // Function to scroll to the desired section
+  void _scrollToSection(GlobalKey key) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final context = key.currentContext;
+      if (context != null) {
+        Scrollable.ensureVisible(
+          context,
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+        );
+      }
+    });
   }
 }
